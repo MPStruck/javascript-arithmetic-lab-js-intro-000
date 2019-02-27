aca3ed3d8439a4534c0688dfd11133ec73b4544a
Michael Struck <mstruck1075@gmail.com>
2019-02-27 18:50:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var a, b
+var a = 10
+
+var b = 5
 
 var c
-
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
 
 fuction add() {
   return a + b
